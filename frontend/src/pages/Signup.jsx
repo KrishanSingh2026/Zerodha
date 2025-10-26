a25a67f744b4beae9e9e3fbe023fad0e17bca148
krishansingh2026 <krishansingh.engineer@gmail.com>
2025-10-26 16:49:37 +0530

    fix bugs

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
-import { useAuth } from "./AuthContext";
+import { useAuth } from "./Authcontext";
 import API_URL from "../config";
 import "./auth.css";
 
